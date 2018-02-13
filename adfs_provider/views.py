c7564ef472af46296fee43736ff20622427487e6
Mikko Keskinen <mikko.keskinen@anders.fi>
2018-02-13 15:06:58 +0200

    Add Tampere ADFS signing certificate

--- adfs_provider/views.py
+++ adfs_provider/views.py
@@ -163,8 +163,25 @@ class TampereADFSOAuth2Adapter(ADFSOAuth2Adapter):
     profile_url = 'https://auth.tampere.fi/user/'
 
     cert = (
-        ''
-        ''
+        'MIIDaTCCAlGgAwIBAgIBATANBgkqhkiG9w0BAQsFADAvMS0wKwYDVQQDEyRBREZT'
+        'IFNpZ25pbmcgLSBzdHMudGFtcGVyZWVuc2V1dHUuZmkwHhcNMTYwNTIzMTA0MjAw'
+        'WhcNMjEwNTIzMTA0MjAwWjAvMS0wKwYDVQQDEyRBREZTIFNpZ25pbmcgLSBzdHMu'
+        'dGFtcGVyZWVuc2V1dHUuZmkwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIB'
+        'AQDBcHZCVOOPGBRTxGYj5LbqPx82zBKyaQ23bTUnlyzvtGgvFs1D6qO7+/JxDGz+'
+        'WnK13KOaLhPxBGgB0Owml8VsisotGqXamCHH9hkQQ0h/rF3YtIfaBj1deC+NujdC'
+        'bB6ArSZoOM9A1RftrvZ7woivxqb5Tt827Xus+AA6MnvTU/odPEzPTlJ24clk1veG'
+        'CpLAcUL5/2HG5ebeL8ixTGtTmJSf0kSoPmq5PwGnadYIGJfsY8xSzzMailWM+kcL'
+        'A8gToJJdLABAD4hjdYkXT5Uib3w6jx1mFN4fLd0XrOSUJtNke0kcI0RXuBxw+/47'
+        'LAFm5mK9yhlwGu1MuhCTWnwRAgMBAAGjgY8wgYwwDAYDVR0TAQH/BAIwADAdBgNV'
+        'HQ4EFgQUdCipMYyoCMrQwAmtbiKgxmSIFpgwCwYDVR0PBAQDAgSwMB0GA1UdJQQW'
+        'MBQGCCsGAQUFBwMBBggrBgEFBQcDAjARBglghkgBhvhCAQEEBAMCBkAwHgYJYIZI'
+        'AYb4QgENBBEWD3hjYSBjZXJ0aWZpY2F0ZTANBgkqhkiG9w0BAQsFAAOCAQEAWrJv'
+        'xHJKaiFu/cDMPDkzZuojXhnKaK9I1IYc5i9lf95B8ixlg86ntpfxzE6c47T6hbkY'
+        '//iOdbVZM9eYP4XPt01bXxkfZ45oQZyjtBnmauxq87ModBMDrZ7mYOodgBTXlxW9'
+        'NCirC7fdC5sC2NcqP8awlOr1kw0xroLnCBAWlGy2y10nLcKk8xmUceiAzTk9zTp5'
+        'HK1BovEtxjfzrgvl6p6DH+ik90guj2jgHNxw+qM2I6QjCa0g5wPqgKSgKxIhlbNe'
+        'iaGjZUWr1pxBKWiPzx3qVqdNRgAWkC1MBd0o+e+QZqsDqrCtGBLFFRQqpqLPw1PO'
+        'caxCehYF/xwqhHys0Q=='
     )
 
     def clean_attributes(self, attrs_in):
